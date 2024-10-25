bf969252012b0ebfcc9c167bfb182db0ba09ca55
paul200234 <paul200234@gmail.com>
2024-10-25 14:08:34 +0200

    Tema 2 ejercicios hasta el 16

--- src/Tema2/Tema2Ejercicio16.java
+++ src/Tema2/Tema2Ejercicio16.java
@@ -1,4 +1,16 @@
 package Tema2;
 
+import java.util.Scanner;
+
 public class Tema2Ejercicio16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Introduce D para convertir euros en dolares");
+        System.out.println("Introduce E para convertir euros en dolares");
+        String opcion = in.next();
+        if(opcion == "D"){
+            Tema2Ejercicio8();
+        }
+    }
 }
