a040953c4d21fb0aea9b19f96d2c0dee7f05b901
paul200234 <paul200234@gmail.com>
2024-10-28 12:15:46 +0100

    Ejercicio1

---
+++ src/Tema3/Tema3Ejercicio1.java
@@ -0,0 +1,11 @@
+package Tema3;
+
+public class Tema3Ejercicio1 {
+    public static void main(String[] args){
+        int i =0;
+        while (i<=1000){
+        System.out.println(i);
+        i+=2;
+        }
+    }
+}
---
+++ src/Tema3/Tema3Ejercicio2.java
@@ -0,0 +1,17 @@
+package Tema3;
+
+import java.util.Scanner;
+
+public class Tema3Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduce el valor entero de A");
+        int a = in.nextInt();
+        System.out.println("Introduce el valor entero de B");
+        int b = in.nextInt();
+        if (b>a){
+
+        }
+
+    }
+}
